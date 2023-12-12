e6c2dcb2a10196aa0fb9f09ff73a7443da717455
Steve John <stevejohnutd@gmail.com>
2023-12-12 02:32:39 +0000

    Input variable block

--- variables.tf
+++ variables.tf
@@ -25,3 +25,21 @@ variable "public_subnets" {
     "public_subnet_3" = 3
   }
 }
+
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the Variables Subnet"
+  type        = string
+  default     = "10.0.202.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used for Variables Subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set Automatic IP Assigment for Variables Subnet"
+  type        = bool
+  default     = true
+}
